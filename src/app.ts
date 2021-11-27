0d892e67be23841053e824ff8331703a59fbcd0c
fedapon <fedapon@gmail.com>
2021-11-27 15:55:22 -0300

    cleanning up the code

--- src/app.ts
+++ src/app.ts
@@ -1,69 +1,25 @@
-import express from 'express'
-import authRoutes from './routes/auth.routes'
-import userRepository from './repositories/user.repositories'
-import dotenv from 'dotenv'
+import express from "express"
+import authRoutes from "./routes/auth.routes"
+import { verifyAuth } from "./middlewares/auth.middlewares"
+import userRepository from "./repositories/user.repositories"
+import dotenv from "dotenv"
 dotenv.config()
-import {ITokenRequest} from './interfaces'
 
 export const Users = new userRepository()
-
 
 const app = express()
 
 //middlewares
-app.use( express.json() )
-app.use( express.urlencoded({extended : false}))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
 
-app.use('/api', authRoutes)
+app.use("/api", authRoutes)
 
-app.get("/", function (req, res) {
-    res.status(200).json({ messaje: "You are at /" })
+app.get("/", verifyAuth, function (req, res) {
+    res.status(200).json({
+        messaje: `Hello ${req.body.username} - You are at /`,
+    })
 })
-
-// app.post("/api/login", function (req, res) {
-//     const user = {
-//         email: req.body.email,
-//     }
-//     const response: any = {}
-//     jwt.sign(user, SECRET, {expiresIn: '1d'}, (error, token) => {
-//         if (error) {
-//             response.messaje = error.message
-//             res.status(403).json(response)
-//             return
-//         }
-//         response.messaje = "You are authorized"
-//         response.token = token
-//         return res.status(200).json(response)
-//     })
-// })
-
-// app.get("/api/resource", verifyToken, function (req: ITokenRequest, res) {
-//     jwt.verify(<string>req.body.token, SECRET, (error, authData) => {
-//         if (error) {
-//             return res.status(403).json({ messaje: "Forbidden" })
-//         }
-//         const response = {
-//             authData,
-//             messaje: "You are authorized",
-//         }
-//         return res.status(200).json(response)
-//     })
-// })
-
-// function verifyToken(
-//     req: ITokenRequest,
-//     res: express.Response,
-//     next: express.NextFunction
-// ) {
-//     const authHeader = req.headers.authorization
-//     if (authHeader !== undefined) {
-//         const bearerToken = authHeader.split(" ")[1]
-//         req.body.token = bearerToken
-//         next()
-//     } else {
-//         return res.status(403).json({ messaje: "Forbidden" })
-//     }
-// }
 
 app.listen(process.env.PORT, function () {
     console.log(`Server started in port: ${process.env.PORT}`)
