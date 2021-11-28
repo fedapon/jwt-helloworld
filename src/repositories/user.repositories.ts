8e01d31d34a588c69d6af17a504cf13e72fcfb6b
fedapon <fedapon@gmail.com>
2021-11-28 12:44:56 -0300

    CRUD finished

--- src/repositories/user.repositories.ts
+++ src/repositories/user.repositories.ts
@@ -14,19 +14,39 @@ export default class userRepository {
         return ++this._nextId
     }
 
+    public create (user : UserEntity) : UserEntity{
+        user.setId(this.getNewId())
+        this._database.push(user)
+        return user
+    }
+
+    public findById (id: Number) : UserEntity{
+        const userFound = this._database.find( (user)=>{
+            return (user.getId()==id)
+        })
+        return userFound as UserEntity
+    }
+
     public findByUsername (username: String) : UserEntity{
         const userFound = this._database.find( (user)=>{
             return (user.getUsername()==username)
         })
         return userFound as UserEntity
     }
-    public create (user : UserEntity) : UserEntity{
-        user.setId(this.getNewId())
-        this._database.push(user)
-        return user
-    }
-    public update () {}
 
-    public delete () {}
+    public update (user : UserEntity) {
+        const newUserList = this._database.filter((item=> {
+            return (user.getId() != item.getId())
+        }))
+        newUserList.push(user)
+        this._database = [...newUserList]
+    }
+
+    public delete (id: Number) {
+        const newUserList = this._database.filter((user=> {
+            return (id != user.getId())
+        }))
+        this._database = [...newUserList]
+    }
 
 }
